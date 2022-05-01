29774f5437a5488018ad36fb121b78bc798e1fe2
gna1024 <nikitahoncharov1024@gmail.com>
2022-05-01 22:01:48 +0300

    Reviews-0

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.reviews, name = "reviews")
+    path("", views.reviews, name = "reviews"),
+    path("add", views.add, name = "add")
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django import forms
+
+reviewss = ["The best coffee", "Great!!!"]
 
 # Create your views here.
 
 def reviews(request):
-    return (render(request, "reviews/reviews.html"))
+    return render(request, "reviews/reviews.html", {
+        "reviews": reviewss
+    })
+
+def add(request):
+    return render(request, "reviews/add.html")
