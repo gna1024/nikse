c53d54c0856b64abc4413d6521916ee9a454fd7f
gna1024 <nikitahoncharov1024@gmail.com>
2022-05-02 15:33:11 +0300

    POST

--- orderahead/views.py
+++ orderahead/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
 order = []
   
 # Create your views here.
 
 def coffee(request):
-    return render(request, 'orderahead/orderahead.html', {
-        "order": order
-    })
+    if request.method =="POST":
+        if 'espresso' in request.POST:
+            order.append("espresso")
+        if 'doppio' in request.POST:
+            order.append("doppio")    
+        if 'americano' in request.POST:
+            order.append("americano")
+        if 'lattemacchiato' in request.POST:
+            order.append("lattemacchiato") 
+        if 'caramelmacchiato' in request.POST:
+            order.append("caramelmacchiato")   
+        if 'latte' in request.POST:
+            order.append("latte")
+        if 'icedlatte' in request.POST:
+            order.append("icedlatte")  
+        if 'cappuccino' in request.POST:
+            order.append("cappuccino")
+        if 'flatwhite' in request.POST:
+            order.append("flatwhite") 
+        if 'irishcoffee' in request.POST:
+            order.append("irishcoffee")    
+    return render(request, "orderahead/orderahead.html")
+    
 
 def cart(request):
+    if request.method =="POST":
+        if 'del' in request.POST:
+            order.clear()
     return render(request, 'cart/cart.html', {
         "order": order,
         "len": len(order)
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
+import datetime
 
-reviewss = ["The best coffee", "Great!!!"]
+reviewss = []
+
+class AddReview(forms.Form):
+    name = forms.CharField(label = "Name", min_length = 1, max_length = 100)
+    review = forms.CharField(label = "Review ", min_length = 1, max_length = 100)
 
 # Create your views here.
 
@@ -11,4 +17,17 @@ def reviews(request):
     })
 
 def add(request):
-    return render(request, "reviews/add.html")
+    if request.method =="POST":
+        form = AddReview(request.POST)
+        if form.is_valid():
+            now = datetime.datetime.now()
+            review = (form.cleaned_data["name"],form.cleaned_data["review"], now.strftime("%d-%m-%y"))
+            reviewss.append(review)
+            return HttpResponseRedirect("/reviews")
+        else: 
+            return render(request, "reviews/add.html",{
+                "form": form
+            })
+    return render(request, "reviews/add.html", {
+        "form": AddReview()
+    })
