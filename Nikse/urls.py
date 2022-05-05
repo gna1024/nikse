66f3aa2a16f5d2dc2c5b06fffff167cb29072c91
gna1024 <nikitahoncharov1024@gmail.com>
2022-05-05 20:30:56 +0300

    Media

--- Nikse/urls.py
+++ Nikse/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from Nikse import settings
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,7 @@ urlpatterns = [
     path('reviews/', include("reviews.urls")),
     path('aboutus/', include("aboutus.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
