66f709d3800bad86821b8cb993ffce8218f9a0cd
gna1024 <nikitahoncharov1024@gmail.com>
2022-05-06 11:54:19 +0300

    Model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=64)
+    review = models.FloatField(max_length=1000)
