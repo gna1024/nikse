2ebb5ea7ed915a64fecac483bef190440e58649c
gna1024 <nikitahoncharov1024@gmail.com>
2022-05-24 18:56:27 +0300

    Java img

--- game/templates/game/game.js
+++ game/templates/game/game.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('form').onsubmit = function(){
         const name = document.querySelector('#name').value;
-        alert(`Hi, ${name}. You find my secret game. If you'll pass all levels`)
+        alert(`Hi, ${name}. You find my secret game. If you pass all levels I will give you a surprise`)
     }
 })
